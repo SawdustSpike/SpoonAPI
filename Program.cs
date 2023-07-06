6955295738d9f99aa50a4ae68552437cdfcb59c3
Michael Cowell <michaelcodesc.gmail.com>
2023-07-05 22:33:35 -0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+var apikey = JObject.Parse(File.ReadAllText("appsettings.json")).GetValue("apiKey").ToString();
+var cat = "recipes";
+var search = "complexSearch";
+var parms = new List<string>() { "query=pasta", "maxFat=25", "number=2" };
+var spoonURL = $"https://api.spoonacular.com/{cat}/{search}?apiKey={apikey}";
+foreach (var param in parms)
+{
+    spoonURL += "&" + param;
+}
+var food = client.GetStringAsync(spoonURL).Result;
+var finalfood = JObject.Parse(food);
+JArray results = (JArray)finalfood["results"];
+foreach (JObject result in results)
+{
+    string title = (string)result["title"];
+    Console.WriteLine(title);
+}
